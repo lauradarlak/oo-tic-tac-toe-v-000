c26ee4787cd481f38e5482a87fba16d12540e77e
Laura Darlak <laura@polyporedesign.com>
2018-08-07 17:31:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,4 +53,17 @@ def current_player
   turn_count % 2 == 0 ? "X" : "O"
 end
 
+#turn
+def turn
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    move(index, current_player(board))
+    display_board
+  else
+    turn
+  end
+end
+
 end
