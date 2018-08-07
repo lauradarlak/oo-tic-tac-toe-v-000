576ebc608cb4fd8698f40b64c2feeabc0989bc0c
Laura Darlak <laura@polyporedesign.com>
2018-08-07 17:06:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,4 +2,16 @@ class TicTacToe
   def initialize(board = Array.new(9, " "))
     @board = board
   end
+
+  WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # First column
+  [1,4,7], # Second column
+  [2,5,8], # Third column
+  [0,4,8], # Left diagonals
+  [2,4,6] # Right diagonals
+]
+
 end
